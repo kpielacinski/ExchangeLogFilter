a7d92abb562fe3d717afc77ccfba6d636e115d1f
Kacper Pielaciński <s9346kp@ms.wwsi.edu.pl>
2021-03-08 23:13:27 +0100

    Dodaj pliki projektów.

---
+++ ExchangeLogFilter/ExchangeLogFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExchangeLogFilter
+{
+    public class ExchangeLogFilter
+    {
+
+        public List<string> Parse(string path, int column_index)
+        {
+            List<string> listA = new List<string>();
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        values[column_index] = values[column_index].Replace("\"", "");
+                        
+                        listA.Add(values[column_index]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+            return listA;
+        }
+
+        private bool isVendor(string name)
+        {
+            return true;
+        }
+    }
+}
---
+++ ExchangeLogFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExchangeLogFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             ExchangeLogFilter filter = new ExchangeLogFilter();
+
+             string path = @"E:\MTSummary_Message trace report.csv";
+
+             List<string> sender_names = filter.Parse(path, 1);
+
+             for (int i = 0; i<=10; i++)
+             {
+                 Console.WriteLine(sender_names[i]);
+             }    
+            
+        }
+    }
+}
